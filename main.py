016269e2503f015dcc7bc5f835eb046a4c5bc817
walissonmdn <walisson.marcelo2000@gmail.com>
2022-12-07 14:29:01 -0300

    Comments translation to English.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#Legenda:
+#Caption:
 #cb: combobox
 #gb: groupbox
 #lb: label
@@ -15,7 +15,7 @@ from functions import *
 from PyQt5 import uic
 from PyQt5.QtWidgets import QMainWindow, QApplication, QMessageBox, QFileDialog
 
-class login(QMainWindow): # Tela de Login
+class login(QMainWindow): # Login Screen
     def __init__(self):
         super(login, self).__init__()
         uic.loadUi("login_window.ui", self)
@@ -23,19 +23,19 @@ class login(QMainWindow): # Tela de Login
         self.pbLogin.clicked.connect(self.check)
     
     def check(self):
-        # Validação
+        # Validation
         if self.leLogin.text() == "":
             QMessageBox.about(self, "Alerta", "Digite o usuário", )
         
         elif self.leSenha.text() == "":
             QMessageBox.about(self, "Alerta", "Digite a senha", )
-        # Passa para a próxima tela
+        # Create an instance of main_windows
         else:
             self.close()
             self.windows_main = main_window(self.leLogin.text(), self.leSenha.text())
-### Fim da classe
+### End of class.
 
-class main_window(QMainWindow):   # Tela para inserir linhas e localização
+class main_window(QMainWindow):   # Fill in the necessary data of a "CT-e"
     def __init__(self, usuario, senha):
         super(main_window, self).__init__()
         uic.loadUi("main_window.ui", self)
@@ -52,14 +52,14 @@ class main_window(QMainWindow):   # Tela para inserir linhas e localização
         self.pbLimpar.clicked.connect(self.limpar)
         self.pbIniciar.clicked.connect(self.iniciar)
 
-    # Configuração de elementos da tela
+    # Initializing the elements of the window.
     def config(self):
-        self.rbInvisible.setVisible(False) # Botão invisível para forçar os outros a serem desmarcados em situações específicas
-        self.rbInvisible_2.setVisible(False) # Botão invisível para forçar os outros a serem desmarcados em situações específicas
+        self.rbInvisible.setVisible(False) # Invisible button to force others to be unchecked in specific situations.
+        self.rbInvisible_2.setVisible(False) # Invisible button to force others to be unchecked in specific situations.
         self.leFatura.setReadOnly(True)
         self.leLocalizacaoCte.setReadOnly(True)
 
-    # Quando o combobox da forma de pagamento for alterado
+    # When "Forma de pagamento" combobox is changed:
     def forma_pagamento_changed(self):
         if self.cbFormadePagamento.currentText() == "Boleto Agrupado":
             self.gbPrimeiraNota.setEnabled(True) 
@@ -89,7 +89,7 @@ class main_window(QMainWindow):   # Tela para inserir linhas e localização
             self.leFatura.setEnabled(False)
             self.leFatura.setText("")
 
-    # Quando o botão "Sim" for marcado ou desmarcado
+    # When "Yes" button is toggled:
     def rbSim_toggled(self):
         if self.rbSim.isChecked() == True:
             self.lbPrimeiroCte.setEnabled(True)
@@ -101,7 +101,7 @@ class main_window(QMainWindow):   # Tela para inserir linhas e localização
             self.inserir_primeiro_cte = True
             self.lePrimeiroCte.setText("")
 
-    # Quando botão "Não" for marcado ou desmarcado
+    # When "No" button is toggled:
     def rbNao_toggled(self):
         if self.rbNao.isChecked() == True:
             self.gbFatura.setEnabled(True)
@@ -113,17 +113,17 @@ class main_window(QMainWindow):   # Tela para inserir linhas e localização
             self.pbProcurarFatura.setEnabled(False)
             self.leFatura.setText("")    
 
-    # Quando botão para procurar fatura for clicado
+    # Button to search for "fatura".
     def encontrar_fatura(self): 
         path = QFileDialog.getOpenFileName()
         path = self.leFatura.setText(path[0])        
 
-    # Quando botão para procurar localização dos documentos de CT-e for clicado
+    # Button to serch for the path of the "CT-e" documents.
     def encontrar_pasta_cte(self):
         path = QFileDialog.getExistingDirectory()
         path = self.leLocalizacaoCte.setText(path)
 
-    # Quando tudo estiver certo, os dados serão especificados e salvos em variáveis
+    # Storing data in variables.
     def salvar_dados(self):
         if self.rbCitroen.isChecked():
             self.unidade = "11.458.618/0001-16"
@@ -158,7 +158,7 @@ class main_window(QMainWindow):   # Tela para inserir linhas e localização
         self.pteCte.setPlainText("")
 
     def iniciar(self):
-        # Validação
+        # Validation.
         if self.rbCitroen.isChecked() == False and self.rbJeep.isChecked() == False and self.rbVolkswagen.isChecked() == False and self.rbParque.isChecked() == False:
             QMessageBox.about(self, "Alerta", "Selecione a unidade.")
         elif self.cbFormadePagamento.currentText() == "":
@@ -177,13 +177,13 @@ class main_window(QMainWindow):   # Tela para inserir linhas e localização
             QMessageBox.about(self, "Alerta", "Insira a localização dos documentos de CT-e.")
         elif self.pteCte.toPlainText() == "":
             QMessageBox.about(self, "Alerta", "Digite o(s) número(s) do(s) documento(s) de CT-e.")
-        # Execução de fato
+        # Create an instance of the automation code.
         else:
             self.salvar_dados()
             self.main = main(self.usuario, self.senha, self.unidade, self.cbFormadePagamento.currentText(), self.inserir_primeiro_cte, self.lePrimeiroCte.text(), self.leDatadeVencimento.text(), self.leFatura.text(), self.leLocalizacaoCte.text(), self.cte_lista)
+### End of class.
 
-
-class main(QMainWindow): # Executa o programa de verificação.
+class main(QMainWindow): # Class to represent the automation code and its settings.
     def __init__(self, usuario, senha, unidade, forma_pagamento, inserir_primeiro_cte, primeiro_cte, data_vencimento, fatura_path, cte_path, cte_lista):
         self.usuario = usuario
         self.senha = senha
@@ -194,20 +194,20 @@ class main(QMainWindow): # Executa o programa de verificação.
         self.fatura_path = fatura_path
         self.cte_path = cte_path
         self.cte_lista = cte_lista
-
-        # Caso primero CT-e já tiver sido inserido, especificar número. Do contrário, programa considerará o número do primeiro que será lançado.
+        
+        # If the first "CT-e" is already in the system, specify its number. Otherwise, the number will be the first "CT-e"'s inserted.
         if inserir_primeiro_cte == True:
             self.primeiro_cte = self.cte_lista[0]
         else:
             self.primeiro_cte = primeiro_cte
 
-        # Quantidade de abas que serão abertas.
+        # The amount of tabs that'll be opened.
         if len(self.cte_lista) < 5:
             self.amount_windows = len(self.cte_lista)
         else:
-            self.amount_windows = 5 # Caso um boleto agrupado tenha mais de 5 documentos de CT-e, apenas 5 abas serão abertas.
+            self.amount_windows = 5 # If there are more than 5 "CT-e" documents, only 5 tabs will be opened.
         
-        # Executa o programa de lançamento automático.
+        # It calls the function to execute the automation code.
         execution = self.run()
         if execution == "cte number is wrong": # Parar o programa caso o arquivo de CT-e esteja com o número diferente do xml.
             msg = QMessageBox()
@@ -216,35 +216,35 @@ class main(QMainWindow): # Executa o programa de verificação.
             msg.exec_()
         
     def run(self):
-        # Inicialização
+        # Initializing browser.
         driver = webdriver.Edge()
         initialize(driver, self.amount_windows, self.usuario, self.senha)
 
-        # Inicialização de variáveis.
-        window_selected = -1 # Haverá incrementação já no primeiro loop
-        repeticao_pagina = 1 # Variável necessária, pois botão de confirmação tem o index mudado a cada lançamento na mesma janela
+        # Initializing variables.
+        window_selected = -1 # There'll be an increase in the first loop.
+        repeticao_pagina = 1 # Necessary variable, since button to a confirmation popup has its index changed each "CT-e" inserted.
         
         last_cte = self.cte_lista[len(self.cte_lista)-1]
 
-        # Executa laço até que todos os documentos de CT-e tenham sido inseridos.
+        # Executing a loop until every "CT-e" is in mySaga
         for cte in self.cte_lista:
 
-            # Seleciona a janela.
+            # Dealing with windows numbers.
             if window_selected == self.amount_windows - 1:
                 window_selected = 0
                 repeticao_pagina+=1 # Index inicial do botão é igual a 1
             else:
                 window_selected+=1
             
-            # Busca chave de acesso no xml do CT-e.
+            # Searching for the xml acess key.
             chave = dados_xml(self.cte_path, cte)
             if chave == False: # Função de cima retorna False caso núm de CT-e no xml esteja diferente do arquivo salvo.
                 return "cte number is wrong"
 
-            # Seleciona janela a ser feita a digitalização.
+            # Selecting window to digitalize a document.
             driver.switch_to.window(driver.window_handles[window_selected])
 
-            #Insere unidade e chave de acesso para digitalização automática.
+            # Inserting "unidade" and "chave de acesso" in order to fill the fields automatically.
             click(driver, '//*[@id="btnCriarPedido"]/span[2]')        
             click(driver, '//*[@id="opcoesDigitalizacao"]/tbody/tr/td[4]/div/div[2]/span')
             fill(driver, '//*[@id="itUnidadeCteDigitalizacao_input"]', self.unidade)
@@ -253,50 +253,50 @@ class main(QMainWindow): # Executa o programa de verificação.
             fill(driver, '//*[@id="itChaveCteDigitalizacao"]', chave)
             click(driver, '//*[@id="btnEnviarCaptchaCteInformar"]/span[2]')
 
-            # Verifica se unidade já está preenchida e se está correta.
+            # Checking if "unidade" is already filled and if so, it checks if it's correct.
             click(driver, '//*[@id="tbViewDigitalizacaoNf:unidadeOrganizacional_input"]')
             string_unidade = driver.find_element(by = By.XPATH, value = '//*[@id="tbViewDigitalizacaoNf:unidadeOrganizacional_input"]').get_attribute("value")
             if self.unidade in string_unidade:
                 pass
             else:
-                driver.find_element(by = By.XPATH, value = '//*[@id="tbViewDigitalizacaoNf:unidadeOrganizacional_input"]').clear() # Apaga valor que foi inserido automaticamente.
-                fill(driver, '//*[@id="tbViewDigitalizacaoNf:unidadeOrganizacional_input"]', self.unidade) # Digita CNPJ da unidade.
-                click(driver, '//*[@id="tbViewDigitalizacaoNf:unidadeOrganizacional_panel"]/table/tbody/tr') # Seleciona a unidade.
-            # Avança.
+                driver.find_element(by = By.XPATH, value = '//*[@id="tbViewDigitalizacaoNf:unidadeOrganizacional_input"]').clear() # Clearing the field to fill in again and make sure it'll be correct.
+                fill(driver, '//*[@id="tbViewDigitalizacaoNf:unidadeOrganizacional_input"]', self.unidade) # Typing "CNPJ".
+                click(driver, '//*[@id="tbViewDigitalizacaoNf:unidadeOrganizacional_panel"]/table/tbody/tr') # Selecting "unidade"
+            # Going to the next step.
             click(driver, '//*[@id="tbViewDigitalizacaoNf:btnProximoNota"]/span[2]')
             time.sleep(0.8)
 
-            # Forma de Pagamento.
-            if self.forma_pagamento == "Boleto Agrupado" and self.inserir_primeiro_cte == True: # Boleto agrupado sem nota lançada.
+            # Payment method.
+            if self.forma_pagamento == "Boleto Agrupado" and self.inserir_primeiro_cte == True: # "Boleto agrupado" and no "CT-e" inserted.
                 select = Select(driver.find_element(by = By.XPATH, value = '//*[@id="tbViewDigitalizacaoNf:soFormaPagamento_input"]'))
                 select.select_by_visible_text('Boleto Agrupado')
 
-            elif self.forma_pagamento == "Boleto Agrupado" and self.inserir_primeiro_cte == False: # Boleto agrupado com nota(s) já lançada(s)
+            elif self.forma_pagamento == "Boleto Agrupado" and self.inserir_primeiro_cte == False: # "Boleto agrupado" and there's already "CT-e" inserted.
                 select = Select(driver.find_element(by = By.XPATH, value = '//*[@id="tbViewDigitalizacaoNf:soFormaPagamento_input"]'))
                 select.select_by_visible_text("Boleto Agrupado")
-                click(driver, '//*[@id="tbViewDigitalizacaoNf:sbPrimeiroBoleto"]') # Muda botão de primiera nota para NÃO
+                click(driver, '//*[@id="tbViewDigitalizacaoNf:sbPrimeiroBoleto"]') # Changing "Primeira nota?" button to "Não"
 
-            elif self.forma_pagamento == "Boleto em Anexo": # Boleto em anexo
+            elif self.forma_pagamento == "Boleto em Anexo": # "Boleto em anexo"
                 select = Select(driver.find_element(by = By.XPATH, value = '//*[@id="tbViewDigitalizacaoNf:soFormaPagamento_input"]'))
                 select.select_by_visible_text("Boleto em anexo")
         
-            # Verifica se label "agência" despareceu para dar continuidade.
+            # Checking if "agência" label disappeared in order to continue.
             while True: 
                 try:
                     driver.find_element(by = By.XPATH, value = '//*[@id="tbViewDigitalizacaoNf:j_idt402"]/div[1]/div[3]/label[1]')
                 except:
                     break    
 
-            # Clica em juros e em CT-e para garantir que campo estará visível.
+            # Clicking on field "juros" and then on "CT-e" to make sure it'll be visible.
             click(driver, '//*[@id="tbViewDigitalizacaoNf:accordionAnexos"]/div[5]') 
             click(driver, '//*[@id="tbViewDigitalizacaoNf:accordionAnexos"]/div[1]')
 
-            # Insere pdf do CT-e.
+            # Inserting "CT-e" pdf.
             fill(driver, '//*[@id="tbViewDigitalizacaoNf:accordionAnexos:j_idt465_input"]', self.cte_path + "/" + cte + ".pdf")
             time.sleep(0.5)
             click(driver, '//*[@id="tbViewDigitalizacaoNf:accordionAnexos:j_idt465"]/div[1]/button[1]/span[2]')
 
-            # Data de vencimento.
+            # "Data de vencimento".
             while True:
                 try:
                     driver.find_element(by = By.XPATH, value = '//*[@id="tbViewDigitalizacaoNf:accordionAnexos:dtAnexoNotaDigitalizacao:0:btnVisualizaAnexoNota"]/span[1]')
@@ -306,62 +306,59 @@ class main(QMainWindow): # Executa o programa de verificação.
                 except:
                     continue
                 
-            # Insere boleto ou agrupa nota.
+            # Inserting documento for payment or typing the first "CT-e" number.
             if self.forma_pagamento == "Boleto em Anexo" or (self.forma_pagamento == "Boleto Agrupado" and self.inserir_primeiro_cte == True):
                 inserir_boleto(driver, self.fatura_path)
                 
             elif self.forma_pagamento == "Boleto Agrupado" and self.inserir_primeiro_cte == False:
                 agrupar_nota(driver, self.primeiro_cte, repeticao_pagina)
             
-            # Avança.
+            # Going to the next step.
             click(driver, '//*[@id="tbViewDigitalizacaoNf:btnProximoRateio"]/span[2]')
             time.sleep(0.5)
 
-            # Inserção de rateio.
-            click(driver, '//*[@id="tbViewDigitalizacaoNf:btnAdicionarRateio"]/span[2]')# Adicionar.
-            # Procura pelo departamento.
-            click(driver, '//*[@id="srCentroCustoRateio_label"]') # Expandir lista de departamentos.
+            # Inserting "rateio".
+            click(driver, '//*[@id="tbViewDigitalizacaoNf:btnAdicionarRateio"]/span[2]')# "Adicionar" button.
+            # Searching for the departament.
+            click(driver, '//*[@id="srCentroCustoRateio_label"]') # Expanding department list.
 
 
             print("Starting now")
             time.sleep(1)
-            for num in range(50): #Procura pelo nome Peças.
+            for num in range(50): # Search for "Peças".
                 centro_rateio = driver.find_element(by = By.XPATH, value = '//*[@id="srCentroCustoRateio_'+str(num)+'"]').text                
                 if "Peças" in centro_rateio:
-                    #driver.find_element(by = By.XPATH, value = '//*[@id="srCentroCustoRateio_input"]').send_keys(num*Keys.ARROW_DOWN)
                     click(driver, '//*[@id="srCentroCustoRateio_'+str(num)+'"]')
                     break
     
             time.sleep(0.5)
 
-            # Procura pelo usuário aprovador.
-            click(driver, '//*[@id="srUsuarioAprovador_label"]') # Expandir lista de usuários
-           # driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
+            # Searching for the user.
+            click(driver, '//*[@id="srUsuarioAprovador_label"]') # Expanding user list.
             time.sleep(1)
-            for num in range(30):   #Procura pelo nome PEDRO FELIPE
+            for num in range(30):   # Search for Pedro
                 nome_rateio = driver.find_element(by = By.XPATH, value = '//*[@id="srUsuarioAprovador_'+str(num)+'"]').text
                 if "PEDRO FELIPE" in nome_rateio:
-                    #driver.find_element(by = By.XPATH, value = '//*[@id="srUsuarioAprovador_input"]').send_keys(num*Keys.ARROW_DOWN)
                     click(driver, '//*[@id="srUsuarioAprovador_'+str(num)+'"]')
                     break
             time.sleep(0.5)
 
-            # Insere valor do rateio e salva
+            # Inserting value of "rateio".
             click(driver, '//*[@id="valorRateio_input"]')
-            fill(driver, '//*[@id="valorRateio_input"]', '100') # 100% do valor
+            fill(driver, '//*[@id="valorRateio_input"]', '100') # 100% of the value.
             click(driver, '//*[@id="btnAdicionarRateio"]/span[2]')
             click(driver, '//*[@id="btnSalvarSolicitacao"]/span[2]')
 
-            # Clica no botão de confirmação para finalizar inserção
+            # Confirming and finishing.
             while True:
                 try:
                     amount_elements = driver.find_elements(by = By.XPATH, value = '//*[@id="j_idt782"]')
-                    amount_elements[-1].click() # Clica no último item da lista amount_elements que é o botão "sim"
+                    amount_elements[-1].click() # Clicking on the last element of the list "amount_elements", which is the "Sim" button.
                     break
                 except:
                     pass
 
-            if self.forma_pagamento == "Boleto Agrupado" and self.inserir_primeiro_cte == True: # Verifica se é a primeira nota e aguarda a inserção ser finalizada.
+            if self.forma_pagamento == "Boleto Agrupado" and self.inserir_primeiro_cte == True: # Verifying if it's the first "CT-e" and awaits until it finishes loading.
                 self.inserir_primeiro_cte = False
                 while True:
                     try:
@@ -370,7 +367,7 @@ class main(QMainWindow): # Executa o programa de verificação.
                         break
                     except:
                         pass
-            elif cte == last_cte: # Aguardar a finalização da última inserção para só então finalizar o navegador.
+            elif cte == last_cte: # Awaits unitil the last "CT-e" insertion is finished to finish the browser.
                 while True:
                     try:
                         click(driver, '//*[@id="btnCriarPedido"]/span[2]')
@@ -378,11 +375,11 @@ class main(QMainWindow): # Executa o programa de verificação.
                     except:
                         pass
 
-###Fim da classe main    
+#End of class.
 
-#Executa a aplicação
+#Execute the software.
 app = QApplication(sys.argv)
-login_window = login() #Instaciamento da tela de login
+login_window = login() # Making an instance from login class.
 app.exec_()
 
 
