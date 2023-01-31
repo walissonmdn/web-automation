6076b8980249b9516f6ac3c9a1aceb95e948b8c5
walissonmdn <walisson.marcelo2000@gmail.com>
2023-01-31 10:43:22 -0300

    New function: insert "CT-e" manually.

--- classes/main.py
+++ classes/main.py
@@ -9,6 +9,7 @@
 from functions import *
 from PyQt5.QtWidgets import QMainWindow, QMessageBox
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchWindowException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 import time
@@ -44,159 +45,170 @@ class main(QMainWindow): # Class to represent the automation code and its settin
             msg.setWindowTitle("Alerta")
             msg.setText("Número de CT-e do XML está diferente do número do arquivo.") # It'll be informed on the screen.
             msg.exec_()
+        elif execution == "O navegador foi encerrado pelo usuário":
+            msg = QMessageBox()
+            msg.setWindowTitle("Alerta")
+            msg.setText("O navegador foi encerrado pelo usuário.") # It'll be informed on the screen.
+            msg.exec_()
         
     def run(self):
-        # Initialize browser.
-        driver = webdriver.Edge()
-        initialize(driver, self.amount_windows, self.usuario, self.senha)
+        try:
+            # Initialize browser.
+            driver = webdriver.Edge()
+            initialize(driver, self.amount_windows, self.usuario, self.senha)
 
-        # Initialize variables.
-        window_selected = -1 # There'll be an increase in the first loop.        
-        last_cte = self.cte_lista[len(self.cte_lista)-1]
+            # Initialize variables.
+            window_selected = -1 # There'll be an increase in the first loop.        
+            last_cte = self.cte_lista[len(self.cte_lista)-1]
 
-        # Execute a loop until every "CT-e" is in mySaga
-        for cte in self.cte_lista:
+            # Execute a loop until every "CT-e" is in mySaga
+            for cte in self.cte_lista:
 
-            # Deal with numbers of the windows.
-            if window_selected == self.amount_windows - 1:
-                window_selected = 0
-            else:
-                window_selected+=1
-            
-            # Search for the xml acess key.
-            chave = dados_xml(self.cte_path, cte)
-            if chave == False: # If "chave" is not found, return.
-                return "cte number is wrong"
+                # Deal with numbers of the windows.
+                if window_selected == self.amount_windows - 1:
+                    window_selected = 0
+                else:
+                    window_selected+=1                
 
-            # Select window to digitalize a document.
-            driver.switch_to.window(driver.window_handles[window_selected])
+                # Select window to digitalize a document.
+                driver.switch_to.window(driver.window_handles[window_selected])
 
-            # Insert "unidade" and "chave de acesso" in order to fill the fields automatically.
-            click(driver, 'button#btnCriarPedido')
-            click(driver, 'table#opcoesDigitalizacao > tbody > tr > td:nth-child(4) > div > div.ui-radiobutton-box.ui-widget.ui-corner-all.ui-state-default')
-            fill(driver, 'input#itUnidadeCteDigitalizacao_input', self.unidade)
-            click(driver, "span#itUnidadeCteDigitalizacao_panel > table > tbody > tr[data-item-label*='"+self.unidade+"']")
-            time.sleep(0.5)
-            fill(driver, 'input#itChaveCteDigitalizacao', chave)
-            click(driver, 'button#btnEnviarCaptchaCteInformar')
+                # Search for the xml acess key.
+                dados_xml = read_xml(self.cte_path, cte)
+                if dados_xml[0] == False: # If "chave" is not found, return.
+                    return "cte number is wrong"
 
-            # Checking if "unidade" is already filled and if so, it checks if it's correct.
-            click(driver, 'input#tbViewDigitalizacaoNf\:unidadeOrganizacional_input')
-            string_unidade = driver.find_element(by = By.CSS_SELECTOR, value = 'input#tbViewDigitalizacaoNf\:unidadeOrganizacional_input')
-            if self.unidade in string_unidade.get_attribute("value"):
-                pass
-            else:
-                string_unidade.clear() # Clear the field to fill in again and make sure it'll be correct.
-                fill(driver, 'input#tbViewDigitalizacaoNf\:unidadeOrganizacional_input', self.unidade) # Typing "CNPJ".
-                click(driver, "span#tbViewDigitalizacaoNf\:unidadeOrganizacional_panel > table > tbody > tr[data-item-label*='"+self.unidade+"']") # Selecting "unidade"
-            
-            # Go to the next step.
-            click(driver, 'button#tbViewDigitalizacaoNf\:btnProximoNota')
-            time.sleep(0.8)
+                # Insert "unidade" and "chave de acesso" in order to fill the fields automatically.
+                click(driver, 'button#btnCriarPedido')
+                click(driver, 'table#opcoesDigitalizacao > tbody > tr > td:nth-child(4) > div > div.ui-radiobutton-box.ui-widget.ui-corner-all.ui-state-default')
+                fill(driver, 'input#itUnidadeCteDigitalizacao_input', self.unidade)
+                click(driver, "span#itUnidadeCteDigitalizacao_panel > table > tbody > tr[data-item-label*='"+self.unidade+"']")
+                time.sleep(0.5)
+                fill(driver, 'input#itChaveCteDigitalizacao', dados_xml[0])
+                click(driver, 'button#btnEnviarCaptchaCteInformar')
 
-            # Payment method.
-            select = Select(driver.find_element(by = By.CSS_SELECTOR, value = 'select#tbViewDigitalizacaoNf\:soFormaPagamento_input'))
-            if self.forma_pagamento == "Boleto Agrupado" and self.inserir_primeiro_cte == True: # "Boleto agrupado" and no "CT-e" inserted.
-                select.select_by_visible_text('Boleto Agrupado')
-
-            elif self.forma_pagamento == "Boleto Agrupado" and self.inserir_primeiro_cte == False: # "Boleto agrupado" and there's already "CT-e" inserted.
-                select.select_by_visible_text("Boleto Agrupado")
-                click(driver, 'div#tbViewDigitalizacaoNf\:sbPrimeiroBoleto') # Chang "Primeira nota?" button to "Não"
-
-            elif self.forma_pagamento == "Boleto em Anexo": # "Boleto em anexo"
-                select.select_by_visible_text("Boleto em anexo")
-        
-            # Check if "agência" label disappeared in order to continue.
-            while True: 
-                try:
-                    driver.find_element(by = By.css, value = "div#tbViewDigitalizacaoNf\:j_idt415 > div:nth-child(1) > div:nth-child(3) > label[for='tbViewDigitalizacaoNf:itAgencia']")
-                except:
-                    break
-
-            # Click on field "juros" and then on "CT-e" to make sure it'll be visible.
-            click(driver, 'div#tbViewDigitalizacaoNf\:accordionAnexos > div:nth-child(5)') 
-            click(driver, 'div#tbViewDigitalizacaoNf\:accordionAnexos > div:nth-child(1)')
-
-            # Insert "CT-e" pdf.
-                               
-            fill(driver, 'div#tbViewDigitalizacaoNf\:accordionAnexos\:pnAnexosNota_content > div.ui-fileupload.ui-widget.ui-fileupload-responsive > div.ui-fileupload-buttonbar.ui-widget-header.ui-corner-top > span > input[type=file]', self.cte_path + "/" + cte + ".pdf")
-            time.sleep(0.5)
-            click(driver, 'div#tbViewDigitalizacaoNf\:accordionAnexos\:pnAnexosNota_content > div.ui-fileupload.ui-widget.ui-fileupload-responsive > div.ui-fileupload-buttonbar.ui-widget-header.ui-corner-top > button.ui-button.ui-widget.ui-state-default.ui-corner-all.ui-button-text-icon-left.ui-fileupload-upload')
-
-            # "Data de vencimento".
-            while True:
-                try:
-                    driver.find_element(by = By.CSS_SELECTOR, value = 'button#tbViewDigitalizacaoNf\:accordionAnexos\:dtAnexoNotaDigitalizacao\:0\:btnVisualizaAnexoNota')
-                    fill(driver, 'input#tbViewDigitalizacaoNf\:calDataVencimento_input', self.data_vencimento)
-                    click(driver, 'div#tbViewDigitalizacaoNf\:accordionAnexos > div:nth-child(3)')
-                    break
-                except:
-                    continue
-
-            # Insert documento for payment or typing the first "CT-e" number.
-            if self.forma_pagamento == "Boleto em Anexo" or (self.forma_pagamento == "Boleto Agrupado" and self.inserir_primeiro_cte == True):
-                inserir_boleto(driver, self.fatura_path)
+                while True:
+                    try:
+                        driver.find_element(by = By.CSS_SELECTOR, value = "input#tbViewDigitalizacaoNf\:itNumeroNota").click() # Verify whether the "CT-e" was imported automatically
+                        digitalizacao_automatica_cte(driver, self.unidade)
+                        break
+                    except:
+                        try:
+                            driver.find_element(by = By.CSS_SELECTOR, value = "button#btnCriarPedido").click()
+                            digitalizacao_manual_cte(driver, cte, self.unidade, dados_xml)
+                            break
+                        except:
+                            pass
                 
-            elif self.forma_pagamento == "Boleto Agrupado" and self.inserir_primeiro_cte == False:
-                agrupar_nota(driver, self.primeiro_cte)
+                # Go to the next step.
+                click(driver, 'button#tbViewDigitalizacaoNf\:btnProximoNota')
+                time.sleep(0.8)
+
+                # Payment method.
+                select = Select(driver.find_element(by = By.CSS_SELECTOR, value = 'select#tbViewDigitalizacaoNf\:soFormaPagamento_input'))
+                if self.forma_pagamento == "Boleto Agrupado" and self.inserir_primeiro_cte == True: # "Boleto agrupado" and no "CT-e" inserted.
+                    select.select_by_visible_text('Boleto Agrupado')
+
+                elif self.forma_pagamento == "Boleto Agrupado" and self.inserir_primeiro_cte == False: # "Boleto agrupado" and there's already "CT-e" inserted.
+                    select.select_by_visible_text("Boleto Agrupado")
+                    click(driver, 'div#tbViewDigitalizacaoNf\:sbPrimeiroBoleto') # Change "Primeira nota?" button to "Não"
+
+                elif self.forma_pagamento == "Boleto em Anexo": # "Boleto em anexo"
+                    select.select_by_visible_text("Boleto em anexo")
             
-            # Go to the next step.
-            click(driver, 'button#tbViewDigitalizacaoNf\:btnProximoRateio')
-            time.sleep(0.5)
+                # Check if "agência" label disappeared in order to continue.
+                while True: 
+                    try:
+                        driver.find_element(by = By.css, value = "div#tbViewDigitalizacaoNf\:j_idt415 > div:nth-child(1) > div:nth-child(3) > label[for='tbViewDigitalizacaoNf:itAgencia']")
+                    except:
+                        break
 
-            # Insert "rateio".
-            click(driver, 'button#tbViewDigitalizacaoNf\:btnAdicionarRateio')# "Adicionar" button.
+                # Click on field "juros" and then on "CT-e" to make sure it'll be visible.
+                click(driver, 'div#tbViewDigitalizacaoNf\:accordionAnexos > div:nth-child(5)') 
+                click(driver, 'div#tbViewDigitalizacaoNf\:accordionAnexos > div:nth-child(1)')
 
-            # Search for the departament.
-            click(driver, 'label#srCentroCustoRateio_label') # Expand department list.
-            time.sleep(1)
-            for num in range(50): # Search for "Peças".
-                centro_rateio = driver.find_element(by = By.CSS_SELECTOR, value = 'li#srCentroCustoRateio_'+str(num)).text                
-                if "Peças" in centro_rateio:
-                    click(driver, 'li#srCentroCustoRateio_'+str(num))
-                    break
-    
-            time.sleep(0.5)
+                # Insert "CT-e" pdf.
+                                
+                fill(driver, 'div#tbViewDigitalizacaoNf\:accordionAnexos\:pnAnexosNota_content > div.ui-fileupload.ui-widget.ui-fileupload-responsive > div.ui-fileupload-buttonbar.ui-widget-header.ui-corner-top > span > input[type=file]', self.cte_path + "/" + cte + ".pdf")
+                time.sleep(0.5)
+                click(driver, 'div#tbViewDigitalizacaoNf\:accordionAnexos\:pnAnexosNota_content > div.ui-fileupload.ui-widget.ui-fileupload-responsive > div.ui-fileupload-buttonbar.ui-widget-header.ui-corner-top > button.ui-button.ui-widget.ui-state-default.ui-corner-all.ui-button-text-icon-left.ui-fileupload-upload')
 
-            # Search for the user. 
-            click(driver, 'label#srUsuarioAprovador_label') # Expand user list.
-            time.sleep(1)
-            for num in range(30):   # Search for Pedro #srUsuarioAprovador_1
-                nome_rateio = driver.find_element(by = By.CSS_SELECTOR, value = 'li#srUsuarioAprovador_'+str(num)).text
-                if "PEDRO FELIPE FERREIRA LEITE" in nome_rateio:
-                    click(driver, 'li#srUsuarioAprovador_'+str(num))
-                    break
-            time.sleep(0.5)
-
-            # Insert value of "rateio".
-            click(driver, 'input#valorRateio_input')
-            fill(driver, 'input#valorRateio_input', '100') # 100% of the value.
-            click(driver, 'button#btnAdicionarRateio')
-            click(driver, 'button#btnSalvarSolicitacao')
-
-            # Confirm and finish.
-            while True:
-                try:
-                    amount_elements = driver.find_elements(by = By.CSS_SELECTOR, value = 'button#j_idt781')
-                    amount_elements[-1].click() # Click on the last element of the list "amount_elements", which is the "Sim" button.
-                    break
-                except:
-                    pass
-
-            if self.forma_pagamento == "Boleto Agrupado" and self.inserir_primeiro_cte == True: # Verify if it's the first "CT-e" and awaits until it finishes loading.
-                self.inserir_primeiro_cte = False
+                # "Data de vencimento".
                 while True:
                     try:
-                        click(driver, '#btnCriarPedido')
-                        driver.refresh()
+                        driver.find_element(by = By.CSS_SELECTOR, value = 'button#tbViewDigitalizacaoNf\:accordionAnexos\:dtAnexoNotaDigitalizacao\:0\:btnVisualizaAnexoNota')
+                        fill(driver, 'input#tbViewDigitalizacaoNf\:calDataVencimento_input', self.data_vencimento)
+                        click(driver, 'div#tbViewDigitalizacaoNf\:accordionAnexos > div:nth-child(3)')
+                        break
+                    except:
+                        continue
+
+                # Insert documento for payment or typing the first "CT-e" number.
+                if self.forma_pagamento == "Boleto em Anexo" or (self.forma_pagamento == "Boleto Agrupado" and self.inserir_primeiro_cte == True):
+                    inserir_boleto(driver, self.fatura_path)
+                    
+                elif self.forma_pagamento == "Boleto Agrupado" and self.inserir_primeiro_cte == False:
+                    agrupar_nota(driver, self.primeiro_cte)
+                
+                # Go to the next step.
+                click(driver, 'button#tbViewDigitalizacaoNf\:btnProximoRateio')
+                time.sleep(0.5)
+
+                # Insert "rateio".
+                click(driver, 'button#tbViewDigitalizacaoNf\:btnAdicionarRateio')# "Adicionar" button.
+
+                # Search for the departament.
+                click(driver, 'label#srCentroCustoRateio_label') # Expand department list.
+                time.sleep(1)
+                for num in range(50): # Search for "Peças".
+                    centro_rateio = driver.find_element(by = By.CSS_SELECTOR, value = 'li#srCentroCustoRateio_'+str(num)).text                
+                    if "Peças" in centro_rateio:
+                        click(driver, 'li#srCentroCustoRateio_'+str(num))
+                        break
+        
+                time.sleep(0.5)
+
+                # Search for the user. 
+                click(driver, 'label#srUsuarioAprovador_label') # Expand user list.
+                time.sleep(1)
+                for num in range(30):   # Search for Pedro #srUsuarioAprovador_1
+                    nome_rateio = driver.find_element(by = By.CSS_SELECTOR, value = 'li#srUsuarioAprovador_'+str(num)).text
+                    if "PEDRO FELIPE FERREIRA LEITE" in nome_rateio:
+                        click(driver, 'li#srUsuarioAprovador_'+str(num))
+                        break
+                time.sleep(0.5)
+
+                # Insert value of "rateio".
+                click(driver, 'input#valorRateio_input')
+                fill(driver, 'input#valorRateio_input', '100') # 100% of the value.
+                click(driver, 'button#btnAdicionarRateio')
+                click(driver, 'button#btnSalvarSolicitacao')
+
+                # Confirm and finish.
+                while True:
+                    try:
+                        amount_elements = driver.find_elements(by = By.CSS_SELECTOR, value = 'button#j_idt789')
+                        amount_elements[-1].click() # Click on the last element of the list "amount_elements", which is the "Sim" button.
                         break
                     except:
                         pass
-            elif cte == last_cte: # Awaits until the last "CT-e" insertion is finished to finish the browser.
-                while True:
-                    try:
-                        click(driver, '#btnCriarPedido')
-                        break
-                    except:
-                        pass
+
+                if self.forma_pagamento == "Boleto Agrupado" and self.inserir_primeiro_cte == True: # Verify if it's the first "CT-e" and awaits until it finishes loading.
+                    self.inserir_primeiro_cte = False
+                    while True:
+                        try:
+                            click(driver, '#btnCriarPedido')
+                            driver.refresh()
+                            break
+                        except:
+                            pass
+                elif cte == last_cte: # Awaits until the last "CT-e" insertion is finished to finish the browser.
+                    while True:
+                        try:
+                            click(driver, '#btnCriarPedido')
+                            break
+                        except:
+                            pass
+        except NoSuchWindowException:
+            return "O navegador foi encerrado pelo usuário"
 #End of class "main".
--- functions.py
+++ functions.py
@@ -1,7 +1,8 @@
-import time
-from selenium.webdriver.common.by import By
-from xml.dom import minidom
 from selenium.common.exceptions import NoSuchElementException, ElementNotInteractableException, StaleElementReferenceException, ElementClickInterceptedException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+from xml.dom import minidom
 
 def initialize(driver, amount_windows, usuario, senha): # Initialize browser.
     driver.get("https://mysaga.gruposaga.com.br/public/index.jsf")
@@ -20,25 +21,16 @@ def click(driver, css_selector): # Click on the specified element.
         try:
             driver.find_element(by = By.CSS_SELECTOR, value = css_selector).click()
             break
-        except NoSuchElementException:
+        except (NoSuchElementException, ElementNotInteractableException, StaleElementReferenceException, ElementClickInterceptedException):
             pass
-        except ElementNotInteractableException:
-            pass
-        except StaleElementReferenceException:
-            pass
-        except ElementClickInterceptedException:
-            pass
+
 
 def fill(driver, css_selector, text): # Fill in a field or upload a file.
     while True:
         try:
             driver.find_element(by = By.CSS_SELECTOR, value = css_selector).send_keys(text)
             break
-        except NoSuchElementException:
-            pass
-        except ElementNotInteractableException:
-            pass
-        except StaleElementReferenceException:
+        except (NoSuchElementException, ElementNotInteractableException, StaleElementReferenceException):
             pass
 
 def inserir_boleto(driver, fatura_path):
@@ -57,13 +49,103 @@ def agrupar_nota(driver, primeiro_cte):
 
     while True:
         try:
-            amount_elements = driver.find_elements(by = By.CSS_SELECTOR, value = 'button#j_idt781')
+            amount_elements = driver.find_elements(by = By.CSS_SELECTOR, value = 'button#j_idt789')
             amount_elements[-1].click() # Click on the list item of amount_elements list, which is the "Sim" button.
             break
         except:
             pass
-           
-def dados_xml(cte_path, cte): # Read xml.
+
+def digitalizacao_automatica_cte(driver, unidade):
+    string_unidade = driver.find_element(by = By.CSS_SELECTOR, value = 'input#tbViewDigitalizacaoNf\:unidadeOrganizacional_input')
+    if unidade in string_unidade.get_attribute("value"):
+        pass
+    else:
+        string_unidade.clear() # Clear the field to fill in again and make sure it'll be correct.
+        string_unidade.send_keys(unidade)
+        #fill(driver, 'input#tbViewDigitalizacaoNf\:unidadeOrganizacional_input', unidade) # Typing "CNPJ".
+        click(driver, "span#tbViewDigitalizacaoNf\:unidadeOrganizacional_panel > table > tbody > tr[data-item-label*='"+unidade+"']") # Selecting "unidade"
+    
+    #Check whether "Serviço/Produto" is already selected and if so, it checks if it's correct.
+    while True:
+        try:
+            string_servico = driver.find_element(by = By.CSS_SELECTOR, value = "label#tbViewDigitalizacaoNf\:srServicoFornecedor_label").text
+            break
+        except StaleElementReferenceException:
+            pass
+
+    if "frete" in string_servico.lower():
+        pass
+    else:
+        try:
+            for num in range(5):
+                servico = driver.find_element(by = By.CSS_SELECTOR, value = "li#tbViewDigitalizacaoNf\:srServicoFornecedor_"+str(num)).text
+                if "frete" in servico.lower():
+                    click(driver, "label#tbViewDigitalizacaoNf\:srServicoFornecedor_label")
+                    click(driver, "li#tbViewDigitalizacaoNf\:srServicoFornecedor_"+str(num))
+                    break
+        except:
+            click(driver, "button#tbViewDigitalizacaoNf\:btnBuscarFornecedor")
+            click(driver, "label#tbViewDigitalizacaoNf\:srServicoFornecedor_label")
+            time.sleep(1)
+            try:
+                for num in range(5):
+                    servico = driver.find_element(by = By.CSS_SELECTOR, value = "li#tbViewDigitalizacaoNf\:srServicoFornecedor_"+str(num)).text
+                    if "frete" in servico.lower():
+                        click(driver, "li#tbViewDigitalizacaoNf\:srServicoFornecedor_"+str(num))
+                        break
+            except:
+                for num in range(5):
+                    servico = driver.find_element(by = By.CSS_SELECTOR, value = "li#tbViewDigitalizacaoNf\:srServicoFornecedor_"+str(num)).text
+                    if "não cadastrado" in servico.lower():
+                        click(driver, "li#tbViewDigitalizacaoNf\:srServicoFornecedor_"+str(num))
+                        break
+
+
+def digitalizacao_manual_cte(driver, cte, unidade, dados_xml):
+    click(driver, "button#j_idt717")
+    fill(driver, 'input#tbViewDigitalizacaoNf\:unidadeOrganizacional_input', unidade)
+    click(driver, "span#tbViewDigitalizacaoNf\:unidadeOrganizacional_panel > table > tbody > tr[data-item-label*='"+unidade+"']")
+    click(driver, "table#tbViewDigitalizacaoNf\:srTipoNota > tbody > tr > td:nth-child(1) > div > div.ui-radiobutton-box.ui-widget.ui-corner-all.ui-state-default")
+    fill(driver, "input#tbViewDigitalizacaoNf\:itChaveNota", dados_xml[0]+Keys.TAB)
+    time.sleep(0.5)
+
+    #Type the value of the "CT-e"
+    valor = driver.find_element(by = By.CSS_SELECTOR, value = "input#tbViewDigitalizacaoNf\:itValorNota")
+    valor.clear()
+    valor.send_keys(dados_xml[1])
+    
+    fill(driver, "input#tbViewDigitalizacaoNf\:itCnpjFornecedor", dados_xml[2]) 
+    click(driver, "button#tbViewDigitalizacaoNf\:btnBuscarFornecedor")
+    time.sleep(0.5)
+    fill(driver, "input#tbViewDigitalizacaoNf\:itNumeroNota", cte)
+    fill(driver, "input#tbViewDigitalizacaoNf\:itNumeroSerie", dados_xml[3])
+    fill(driver, "input#tbViewDigitalizacaoNf\:calDataEmissao_input", dados_xml[4]+Keys.TAB)
+    time.sleep(0.5)
+
+    #Select "Serviço/Produto"
+    click(driver, "label#tbViewDigitalizacaoNf\:srServicoFornecedor_label")
+    time.sleep(0.5)
+    while True:
+        try:
+            for num in range(5):
+                servico = driver.find_element(by = By.CSS_SELECTOR, value = "li#tbViewDigitalizacaoNf\:srServicoFornecedor_"+str(num)).text
+                if "frete" in servico.lower():
+                    click(driver, "li#tbViewDigitalizacaoNf\:srServicoFornecedor_"+str(num))
+                    exit_loop = True
+                    break
+                elif "não cadastrado" in servico.lower():
+                    click(driver, "li#tbViewDigitalizacaoNf\:srServicoFornecedor_"+str(num))
+                    exit_loop = True
+                    break
+            if exit_loop == True:
+                break
+        except:
+            pass
+
+    click(driver, "textarea#tbViewDigitalizacaoNf\:itaDescricao")
+    fill(driver, "textarea#tbViewDigitalizacaoNf\:itaDescricao", "Frete"+Keys.TAB)
+
+def read_xml(cte_path, cte): # Read xml.
     xml = open(cte_path + "/" + cte + ".xml")
     cte_parse = minidom.parse(xml)
     cte_num = cte_parse.getElementsByTagName("nCT")
@@ -76,4 +158,19 @@ def dados_xml(cte_path, cte): # Read xml.
     else:
         chave = False
 
-    return chave
+    valor_cte = cte_parse.getElementsByTagName('vRec')
+    valor_cte = valor_cte[0].firstChild.data
+
+    cnpj = cte_parse.getElementsByTagName("CNPJ")
+    cnpj = cnpj[0].firstChild.data
+
+    serie = cte_parse.getElementsByTagName("serie")
+    serie = serie[0].firstChild.data
+
+    # Busca pela data de emissão
+    data_emissao = cte_parse.getElementsByTagName('dhEmi')
+    data_emissao = (data_emissao[0].firstChild.data)
+    #yyyy-mm-dd to dd-mm-yyyy
+    data_emissao = data_emissao[8] + data_emissao[9] + '/' + data_emissao[5] + data_emissao[6] + '/' + data_emissao[0] + data_emissao[1] + data_emissao[2] + data_emissao[3]
+
+    return [chave, valor_cte, cnpj, serie, data_emissao]
